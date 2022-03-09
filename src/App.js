cf79a93fe07e010d78074e3c9861731bb84a691a
Alexander-Yarnell <yarnell.alexander@yahoo.com>
2022-03-09 10:00:30 +0000

    Cleaning up react files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>Hello</div>
-      </header>
-    </div>
+    <div>Barnhart</div>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Nav(){
+  return(<div>This is my navbar</div>);
+}
+
+export default Nav;
